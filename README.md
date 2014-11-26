759c8042bb66b359f3c7f21882428bf2713d69c4
coderaiser <mnemonic.enemy@gmail.com>
2014-11-26 05:38:12 -0500

    feature(jsconsole) add

---
+++ README.md
@@ -0,0 +1,18 @@
+JSConsole
+=======
+
+JavaScript web console based on [console-io](https://github.com/cloudcmd/console).
+
+![Console](https://raw.githubusercontent.com/coderaiser/node-jsconsole/master/img/jsconsole.png "Console")
+
+## Install
+
+`npm i jsconsole -g`
+
+## Use
+
+Start `jsconsole`, go to url `http://localhost:1337`
+
+## License
+
+MIT
---
+++ bin/jsconsole.js
@@ -0,0 +1,100 @@
+#!/usr/bin/env node
+
+(function() {
+    'use strict';
+    
+    var argv        = process.argv,
+        path        = require('path'),
+        exec        = require('execon'),
+        vm          = require('vm'),
+        
+        DIR         = __dirname + '/../',
+        Index       = path.join(DIR + 'html/index.html'),
+        
+        Clients     = [],
+        Num         = 0,
+        
+        argvLast    = argv.slice().pop();
+    
+    switch (argvLast) {
+    case '-v':
+        version();
+        break;
+    
+    case '--v':
+        version();
+        break;
+    
+    default:
+        start();
+    }
+    
+    function start() {
+        var webconsole  = require('console-io'),
+            http        = require('http'),
+            
+            express     = require('express'),
+            minify      = require('minify'),
+            
+            app         = express(),
+            server      = http.createServer(app),
+            
+            port        =   process.env.PORT            ||  /* c9           */
+                            process.env.app_port        ||  /* nodester     */
+                            process.env.VCAP_APP_PORT   ||  /* cloudfoundry */
+                            1337,
+            
+            ip          =   process.env.IP              ||  /* c9           */
+                            '0.0.0.0';
+        
+        app .use(webconsole({
+                server: server,
+                execute: execute,
+                prompt: ' ',
+                online: false
+            }))
+            .use(minify({
+                dir: DIR
+            }))
+            .use(express.static(DIR))
+            
+            .get('/', function (req, res) {
+                res.sendFile(Index, function(error) {
+                    if (error)
+                         res
+                            .status(error.status)
+                            .end();
+                });
+            });
+            
+        server.listen(port, ip);
+        
+        console.log('url: http://' + ip + ':' + port);
+    }
+    
+    function version() {
+        var pack = require('../package.json');
+        
+        console.log('v' + pack.version);
+    }
+    
+    function execute(socket, code) {
+        var error, context;
+        
+        if (!Clients[Num])
+            Clients[Num] = {
+                context : vm.createContext()
+            };
+        
+        context = Clients[Num].context;
+        
+        error   = exec.try(function() {
+            vm.runInContext('result = eval("' + code + '")', context);
+        });
+        
+        if (error)
+            socket.emit('err', error.message + '\n');
+        else
+            socket.emit('data', context.result + '\n');
+    }
+})();
